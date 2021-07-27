0c26ce84f1336a6dc54e9f1a8d83afd360f6c3cc
Alex Fila <filal41sng@gmail.com>
2021-07-27 11:01:34 +0300

    class Client described

---
+++ client.py
@@ -0,0 +1,13 @@
+class Client:
+    def __init__(self, name, nickname, ip, is_active, email, phone, switch, port, saved_mac, current_mac, port_is_active):
+        self.name = name,
+        self.nickname = nickname,
+        self.ip = ip,
+        self.is_active = is_active,
+        self.email = email,
+        self.phone = phone,
+        self.switch = switch,
+        self.port = port,
+        self.saved_mac = saved_mac,
+        self.current_mac = current_mac,
+        self.port_is_active = port_is_active
