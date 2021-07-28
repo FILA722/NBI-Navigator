61e4d8449add4d58bf84a2a15b47914d16fb81d3
Alex Fila <filal41sng@gmail.com>
2021-07-28 16:23:34 +0300

    clients.py edited

--- client.py
+++ client.py
@@ -1,7 +1,8 @@
 class Client:
-    def __init__(self, name, nickname, ip, is_active, email, phone, switch, port, saved_mac, current_mac, port_is_active):
+    def __init__(self, name, nickname, notes, ip, is_active, email, phone, switch, port, saved_mac, current_mac, port_is_active, errors_on_port, cacti_pic):
         self.name = name,
         self.nickname = nickname,
+        self.notes = notes
         self.ip = ip,
         self.is_active = is_active,
         self.email = email,
@@ -10,4 +11,7 @@ class Client:
         self.port = port,
         self.saved_mac = saved_mac,
         self.current_mac = current_mac,
-        self.port_is_active = port_is_active
+        self.port_is_active = port_is_active,
+        self.errors_on_port = errors_on_port
+        self.cacti_pic = cacti_pic
+
