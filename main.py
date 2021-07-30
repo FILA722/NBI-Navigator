3ea8f543a872c23dab9999607d8718015417c475
Alex Fila <filal41sng@gmail.com>
2021-07-30 04:35:22 +0300

    added get_all_clients_from_netstore

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import parse_netstore
-from client_description.py import Client
+from client_description import Client
 import json
 import os
 
 def main():
     client = input('Введите имя клиента:')
-    if not os.path.isfile('clients.json'):
+    if os.path.isfile('clients.json'):
         parse_netstore.update_clients()
 
     with open('clients.json', 'r') as dict_with_clients:
@@ -15,6 +15,6 @@ def main():
     except KeyError:
         print(f"Client doesn't found")
 
-    parse_netstore.get_client_netstore_info(clients[client])
+# test()
 main()
 # parse_netstore.update_clients()
--- parse_netstore.py
+++ parse_netstore.py
@@ -1,8 +1,65 @@
+import time
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import StaleElementReferenceException
 from start_browser import driver
-from locators import NetstoreLocators
+from locators import NetstoreLocators, NetstoreClientPageLocators
 from confidential import NetstoreLoginData
+import re
 import json
 
+def get_ipaddr_switch_name_port_macAddress_from_client_note(browser, note):
+    client_ip_addresses = tuple(ip_address.text for ip_address in browser.find_elements(*NetstoreClientPageLocators.IP_ADDRESSES))
+
+    switches = re.findall(r'==[A-Za-z0-9-_\/. #]+==', note)
+    switches = tuple(switch.strip('==') for switch in switches)
+    mac_addresses = re.findall(r'\w\w:\w\w:\w\w:\w\w:\w\w:\w\w', note)
+
+    client_connection_data = {}
+    for i in range(len(client_ip_addresses)):
+        client_port = re.findall(r'#\d+', switches[i])
+        client_switch = switches[i].strip(client_port[0])
+        client_mac_address = mac_addresses[i]
+        client_connection_data[client_ip_addresses[i]] = (client_switch, client_port[0], client_mac_address)
+
+    return client_connection_data
+
+# def collect_client_data(browser, clients):
+def collect_client_data(browser, clients):
+    client_data = {}
+    for client in clients:
+        client_name = client[0]
+        client_netstore_url = client[1]
+
+        browser.get(client_netstore_url)
+
+        client_tel = browser.find_element(*NetstoreClientPageLocators.TEL).get_attribute("value")
+        client_email = browser.find_element(*NetstoreClientPageLocators.EMAIL).get_attribute("value")
+        try:
+            client_physical_address = browser.find_element(*NetstoreClientPageLocators.PHYSICAL_ADDRESS).text
+            client_physical_address_notes = browser.find_element(*NetstoreClientPageLocators.PHYSICAL_ADDRESS_NOTES).text
+        except NoSuchElementException:
+            client_physical_address = None
+            client_physical_address_notes = None
+
+        client_is_active = browser.find_element(*NetstoreClientPageLocators.IS_ACTIVE).text
+        client_is_converter = browser.find_element(*NetstoreClientPageLocators.IS_CONVERTER).get_attribute("checked")
+        client_speed = browser.find_element(*NetstoreClientPageLocators.SPEED).get_attribute("value")
+        client_notes = browser.find_element(*NetstoreClientPageLocators.NOTES).text
+        client_connection_data = get_ipaddr_switch_name_port_macAddress_from_client_note(browser, client_notes)
+
+        client_data[client_name] = (
+                                  client_tel,
+                                  client_email,
+                                  client_physical_address,
+                                  client_physical_address_notes,
+                                  client_is_active,
+                                  client_is_converter,
+                                  client_speed,
+                                  client_notes,
+                                  client_connection_data,
+                                  client_netstore_url)
+
+    return client_data
 def get_all_clients_from_netstore(browser, login_, passw):
 
     login = browser.find_element(*NetstoreLocators.LOGIN)
@@ -20,27 +77,32 @@ def get_all_clients_from_netstore(browser, login_, passw):
     dict_of_clients_from_netstore = {}
 
     active_clients_from_netstore = browser.find_elements(*NetstoreLocators.GET_ALL_ACTIVE_CLIENTS_LIST)
+    list_of_active_clients = []
     for active_client in active_clients_from_netstore:
-        client_object = active_client.find_element_by_css_selector('a')
-        dict_of_clients_from_netstore[client_object.text] = client_object.get_attribute("href").replace('_properties', '_client')
+        client_object = active_client.find_element_by_tag_name('a')
+        client_name = client_object.text
+        client_netstore_url = client_object.get_attribute("href").replace('_properties', '_client')
+        list_of_active_clients.append((client_name, client_netstore_url))
+    dict_of_clients_from_netstore.update(collect_client_data(browser, list_of_active_clients))
 
     terminated_clients_from_netstore = browser.find_elements(*NetstoreLocators.GET_ALL_TERMINATED_CLIENTS_LIST)
+    list_of_terminated_clients = []
     for terminated_client in terminated_clients_from_netstore:
-        client_object = terminated_client.find_element_by_css_selector('a')
-        dict_of_clients_from_netstore[client_object.text] = client_object.get_attribute("href").replace('_properties', '_client')
+        client_object = terminated_client.find_element_by_tag_name('a')
+        client_name = client_object.text
+        client_netstore_url = client_object.get_attribute("href").replace('_properties', '_client')
+        list_of_terminated_clients.append((client_name, client_netstore_url))
+    dict_of_clients_from_netstore.update(collect_client_data(browser, list_of_terminated_clients))
 
     return dict_of_clients_from_netstore
-
-def get_client_netstore_info(url):
-    client_page = driver(url)
-
 def update_clients():
     try:
+        clients = {}
         browser = driver(NetstoreLoginData.netstore1_url)
-        clients = get_all_clients_from_netstore(browser,
+        clients.update(get_all_clients_from_netstore(browser,
                                                 NetstoreLoginData.netstore1_login,
                                                 NetstoreLoginData.netstore_passwd,
-                                                )
+                                                ))
 
         browser = driver(NetstoreLoginData.netstore2_url)
         clients.update(get_all_clients_from_netstore(browser,
@@ -51,6 +113,7 @@ def update_clients():
         json_clients_dict = json.dumps(clients, indent=2, sort_keys=True, ensure_ascii=False)
         with open('clients.json', 'w') as dict_with_clients:
             dict_with_clients.write(json_clients_dict)
+
         return 'Обновление списка клиентов успешно завершено'
 
     finally:
