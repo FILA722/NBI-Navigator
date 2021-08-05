5f000d17bf3cec8aa9df49e949a8682e91176e50
Alex Fila <filal41sng@gmail.com>
2021-08-05 23:22:59 +0300

    added parse_zone_data in zones_parser.py

--- zones_parser.py
+++ zones_parser.py
@@ -1,18 +1,31 @@
 from confidential import ZonesLoginData
 import paramiko
-login_data = {
-    'hostname': ZonesLoginData.zones_ip,
-    'username': ZonesLoginData.zones_login,
-    'password': ZonesLoginData.zones_passwd,
-    'port': 22
-}
-client = paramiko.SSHClient()
-client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-client.connect(**login_data)
-stdin, stdout, stderr = client.exec_command('less /etc/namedb/primary/nbi.com.ua')
 
-with open('zones.txt', 'a') as zones:
-    for string in stdout:
-        zones.write(string)
+def get_zone_data():
+    login_data = {
+        'hostname': ZonesLoginData.zones_ip,
+        'username': ZonesLoginData.zones_login,
+        'password': ZonesLoginData.zones_passwd,
+        'port': 22
+    }
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(**login_data)
+    stdin, stdout, stderr = client.exec_command('less /etc/namedb/primary/nbi.com.ua')
 
-client.close()
+    with open('zones.txt', 'a') as zones:
+        for string in stdout:
+            zones.write(string)
+    client.close()
+
+def parse_zone_data():
+    with open('zones.txt') as zones:
+        zones = list(zones)
+
+
+def main():
+    # get_zone_data()
+    parse_zone_data()
+
+main()
+
