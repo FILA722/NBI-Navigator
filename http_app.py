33dbf023f83ec4d379a141535868de9baaa5477e
Alex Fila <filal41sng@gmail.com>
2021-09-06 12:54:56 +0300

    debuged console output maked if rule instead of cycle of current ip-addresses

--- http_app.py
+++ http_app.py
@@ -1,11 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, flash, redirect
+from search_engine import search_engine
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = 'iuywfiyug23poiuj2piou5h2pio53thj2[3io5jtp25'
 
 @app.route('/')
 def search():
     return render_template('search.html')
+
+
+@app.route('/client', methods=('GET', 'POST'))
+def find_client():
+    if request.method == 'POST':
+        client = request.form['client']
+
+        if not client:
+            flash('Client not found')
+        else:
+            coincidence_names = search_engine.search(client)
+            print(coincidence_names)
+            return render_template('client.html', client=coincidence_names)
+
 
 
 if __name__ == '__main__':
