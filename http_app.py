bf5cae129396e98a4cee44334ad0f78aa3545fab
Alex Fila <filal41sng@gmail.com>
2021-09-17 15:31:16 +0300

    made output of conciedence_names on search.html

--- http_app.py
+++ http_app.py
@@ -4,9 +4,18 @@ from search_engine import search_engine
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'iuywfiyug23poiuj2piou5h2pio53thj2[3io5jtp25'
 
-@app.route('/')
+
+@app.route('/', methods=['POST', 'GET'])
 def search():
-    return render_template('search.html')
+    if request.method == 'POST':
+        clients = search_engine.search(request.form['client'])
+
+        if clients == False:
+            return render_template('search.html', clients=['Клиент не найден'])
+        else:
+            return render_template('search.html', clients=clients)
+    else:
+        return render_template('search.html')
 
 
 @app.route('/client/<client_name>')
@@ -16,12 +25,10 @@ def find_client(client_name):
     client_data = search_result[1]
     print(client_name)
     print(client_data)
-    if not search_result:
-        return 'Client not found'
-    else:
-        return client_name
+
+    return render_template('client.html', search_result=search_result)
 
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
