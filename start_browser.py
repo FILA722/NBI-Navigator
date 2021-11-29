45254d9dbf013f44ad9c36cef0c5fe7d9de60405
Alex Fila <filal41sng@gmail.com>
2021-11-29 12:32:09 +0200

    edited work_time added WebDriverExceprion ti the start_browser.py

--- start_browser.py
+++ start_browser.py
@@ -1,12 +1,22 @@
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.common.exceptions import WebDriverException
 from selenium import webdriver
-
+import logging
+import time
 
 def driver(url):
     #Start browser in HEADLESS MODE
     options = FirefoxOptions()
     options.add_argument("--headless")
-    browser = webdriver.Firefox(options=options)
+    try:
+        browser = webdriver.Firefox(options=options)
+        # browser.get(url)
+    except WebDriverException:
+        logging.info(f'WebDriverException with request to the {url}')
+        time.sleep(15)
+        browser = webdriver.Firefox(options=options)
+        logging.info(f'WebDriverException again with request to the {url}')
+
     browser.get(url)
 
     #Start browser in VISUAL MODE
